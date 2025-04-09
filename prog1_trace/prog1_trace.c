5236b0874f01e93b067ca8c1cc84806d9e3b6cab
JGonzo24 <joshuaemmanuelangel@gmail.com>
2025-04-08 20:59:48 -0700

    first push to main from prog1_trace

---
+++ prog1_trace/prog1_trace.c
@@ -0,0 +1,32 @@
+
+#include <stdio.h>
+
+void ethernet()
+{
+
+}
+
+void icmp()
+{
+
+}
+
+void arp()
+{
+
+}
+
+void ip()
+{
+
+}
+
+void tcp()
+{
+    
+}
+
+int main()
+{
+    printf("Hello world\n");
+}
