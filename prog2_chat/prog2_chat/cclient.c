dc6c1e553e94884cb94a01f339571278dc30c06a
JGonzo24 <joshuaemmanuelangel@gmail.com>
2025-04-24 01:03:58 -0700

    handleSendMessage()

--- prog2_chat/prog2_chat/cclient.c
+++ prog2_chat/prog2_chat/cclient.c
@@ -24,31 +24,178 @@
 #include "networks.h"
 #include "safeUtil.h"
 #include "sendreceive.h"
+#include "handle_table.h"
+#include <poll.h> // Include poll.h for pollfd structure
 #include "pollLib.h"
 
 #define MAXBUF 1024
 #define DEBUG_FLAG 1
+#define MAX_MSG_SIZE 200
 
 void sendToServer(int socketNum);
 int readFromStdin(uint8_t * buffer);
 void checkArgs(int argc, char * argv[]);
 void clientControl(int socketNum);
 void processMsgFromServer(int socketNum);
+int chooseCommand(char* buffer);
+CommandType parseCommand(const char* buffer);
+void handleSendMessage(int socketNum, const char* buffer);
+void handleBroadcastMessage(int socketNum, const char* buffer);
+void handleMulticastMessage(int socketNum, const char* buffer);
+void handleListHandles(int socketNum, const char* buffer);
+void handleInvalidCommand(int socketNum, const char* buffer);
+
+
+
+typedef enum
+{
+	CMD_SEND_MESSAGE,
+	CMD_BROADCAST_MESSAGE,
+	CMD_MULTICAST_MESSAGE,
+	CMD_LIST_HANDLES,
+	CMD_INVALID
+} CommandType;
+
+
+CommandType parseCommand(const char* buffer)
+{
+	// Ensure that the command is case insensitive
+	char command[MAXBUF];
+	int i = 0;
+	while (buffer[i] != '\0' && i < MAXBUF - 1)
+	{
+		command[i] = tolower(buffer[i]);
+		i++;
+	}
+	command[i] = '\0'; // Null terminate the string
+
+	if (strncmp(buffer, "%m", 2) == 0) 
+	{
+		return CMD_SEND_MESSAGE;
+	}
+	else if (strncmp(buffer, "%b",2) == 0) 
+	{
+		return CMD_BROADCAST_MESSAGE;
+	}
+	else if (strncmp(buffer, "%c", 2) == 0) 
+	{
+		return CMD_MULTICAST_MESSAGE;
+	}
+	else if (strncmp(buffer, "%l", 2) == 0) 
+	{
+		return CMD_LIST_HANDLES;
+	}
+	else
+	{
+		return CMD_INVALID;
+	}
+}
+
+
+void handleSendMessage(int socketNum, const char* buffer)
+{
+	// First thing to check is the size of the message, if greater than 200 then break it up so you don't send more than 200 bytes
+	int buffer_length = strlen(buffer);
+	char message[MAX_MSG_SIZE];
+	int bytes_sent = 0;
+
+	// Get the PDU ready
+	uint8_t message_packet_len[3];
+	uint8_t flag; 
+	uint8_t sender_handle_len;
+	uint8_t sender_handle[32];
+	uint
+
+
+	while (bytes_sent < buffer_length)
+	{
+		int bytes_to_send = buffer_length - bytes_sent;
+		if (bytes_to_send > MAX_MSG_SIZE)
+		{
+			bytes_to_send = MAX_MSG_SIZE;
+		}
+		
+		strncpy(message, buffer + bytes_sent, bytes_to_send);
+		message[bytes_to_send] = '\0'; // Null terminate the message
+		bytes_sent += bytes_to_send;
+		
+		sendPDU(socketNum, (uint8_t*)message, bytes_to_send);
+		printf("Sent message: %s\n", message);
+	}
+}
 
 int main(int argc, char * argv[])
 {
 	int socketNum = 0;         //socket descriptor
-	
+	char *handle = argv[1];
 	checkArgs(argc, argv);
 
 	/* set up the TCP Client socket  */
-	socketNum = tcpClientSetup(argv[1], argv[2], DEBUG_FLAG);
+	socketNum = tcpClientSetup(argv[2], argv[3], DEBUG_FLAG);
+	
+	printf("Handle: %s\n", handle);
+
+
 	
 	// call the client control() function
 	clientControl(socketNum);
 		
 	return 0;
 }
+
+int chooseCommand(char* buffer)
+{
+	CommandType commandType = parseCommand(buffer);
+	
+	switch (commandType)
+	{
+		case CMD_SEND_MESSAGE:
+			// Handle send message command
+			printf("Send message command detected.\n");
+			
+			break;
+		
+		case CMD_BROADCAST_MESSAGE:
+			// Handle broadcast message command
+			printf("Broadcast message command detected.\n");
+			break;
+
+		case CMD_MULTICAST_MESSAGE:
+			// Handle multicast message command
+			printf("Multicast message command detected.\n");
+			break;
+
+		case CMD_LIST_HANDLES:
+			// Handle list handles command
+			printf("List handles command detected.\n");
+			break;	
+
+		case CMD_INVALID:
+			// Handle invalid command
+			printf("Invalid command detected.\n");
+			break;
+
+		default:
+			// Handle unknown command
+			printf("Unknown command detected.\n");
+			break;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 void clientControl(int socketNum)
 {
@@ -61,6 +208,8 @@ void clientControl(int socketNum)
 	// Continously loop throug hthe to accept user input and process messages from the user
 	while(1)
 	{
+		printf("$: ");
+		fflush(stdout);
 		int returned_socket = pollCall(-1);
 
 		if (returned_socket < 0)
@@ -116,14 +265,16 @@ void sendToServer(int socketNum)
 	int sent = 0;            //actual amount of data sent/* get the data and send it   */
 	int recvBytes = 0;
 	
-	printf("Enter data: \n");
-	fflush(stdout);
+
 	// read from stdin
 	sendLen = readFromStdin(buffer);
-	printf("\nread: %s string len: %d (including null)\n", buffer, sendLen);
+	
+	int command_success = choose_command(buffer); // Returns 0 on success, -1 on failure
+
+	// printf("\nread: %s string len: %d (including null)\n", buffer, sendLen);
 	
 	
-	sent = sendPDU(socketNum, buffer, sendLen);
+	// sent = sendPDU(socketNum, buffer, sendLen);
 
 	if (sent < 0)
 	{
@@ -174,9 +325,9 @@ int readFromStdin(uint8_t * buffer)
 void checkArgs(int argc, char * argv[])
 {
 	/* check command line arguments  */
-	if (argc != 3)
+	if (argc != 4)
 	{
-		printf("usage: %s host-name port-number \n", argv[0]);
+		printf("usage: %s handle host-name port-number \n", argv[0]);
 		exit(1);
 	}
 }
